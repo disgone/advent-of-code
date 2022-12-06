b0e6a7bc3c74b598869a34f6fc04cb4501c68e46
Shaun Smith <shaun.smith@gmfinancial.com>
2022-12-06 12:03:24 -0600

    Add solution for Day 1, part 1

--- 2022/Day01/Program.cs
+++ 2022/Day01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿long currentElf = 0L;
+long maxCalories = 0L;
+await foreach (string line in File.ReadLinesAsync("input.txt"))
+{
+    if (long.TryParse(line, out long calories))
+    {
+        currentElf += calories;
+    }
+    else
+    {
+        if (currentElf > maxCalories)
+        {
+            maxCalories = currentElf;
+        }
+
+        currentElf = 0;
+    }
+}
+
+Console.WriteLine($"Max calories held is {maxCalories}");
