0a2feae69c6dc0bab6a10e5bce339dd9c737bb09
Shaun Smith <shaun.smith@gmfinancial.com>
2022-12-07 13:37:15 -0600

    Fix typo

--- 2022/Day03/Program.cs
+++ 2022/Day03/Program.cs
@@ -1,15 +1,15 @@
-﻿int totalPriorty = 0;
+﻿int totalPriority = 0;
 
 foreach (string line in File.ReadLines("input.txt"))
 {
     var span = line.AsSpan();
     var midPoint = span.Length / 2;
-    
+
     var a  = span.Slice(0, midPoint);
     var b = span.Slice(midPoint);
 
     var dupe = a.ToArray().Intersect(b.ToArray()).First();
-    totalPriorty += dupe - (Char.IsBetween(dupe, 'a', 'z') ? 96 : 38);
+    totalPriority += dupe - (char.IsBetween(dupe, 'a', 'z') ? 96 : 38);
 }
 
-Console.WriteLine("Total priority " + totalPriorty);
+Console.WriteLine("Total priority " + totalPriority);
