dc0aec7d64efc8149aee5b749e66692e2b522c38
Shaun Smith <disgone@gmail.com>
2022-12-08 15:23:41 -0600

    Add solution for day 8, part 1

--- 2022/Day08/Program.cs
+++ 2022/Day08/Program.cs
@@ -5,86 +5,64 @@ int columns = data[0].Length;
 
 Tree[,] grid = new Tree[rows, columns];
 
-int maxHeightY = 0;
-for (int x = 0; x < rows; x++)
+int[] maxHeightY = new int[columns];
+for (int y = 0; y < rows; y++)
 {
-    var line = data[x];
+    var line = data[y];
     int maxHeightX = 0;
-
-    for (int y = 0; y < columns; y++)
+    for (int x = 0; x < columns; x++)
     {
-        int height = int.Parse(line[y].ToString());
+        int height = int.Parse(line[x].ToString());
 
         // The edges of our matrix are visible by default.
-        bool isVisible = x == 0 || y == 0 || x == columns - 1 || y == rows - 1;
+        bool isVisible = y == 0 || y == columns - 1 || x == 0  || x == rows - 1;
 
-        if (height > maxHeightX)
+        if (maxHeightY[x] < height)
         {
+            maxHeightY[x] = height;
             isVisible = true;
+        }
+
+        if (maxHeightX < height)
+        {
             maxHeightX = height;
-        }
-
-        if (height > maxHeightY)
-        {
             isVisible = true;
-            maxHeightY = height;
         }
 
-        grid[x, y] = new Tree(height, isVisible);
+        grid[y, x] = new Tree(height, isVisible);
     }
 }
 
-
-// Set visibility from right-left, bottom-top
-maxHeightY = 0;
-for (int x = grid.GetUpperBound(1); x >= 0; x--)
+maxHeightY = new int[columns];
+for (int y = rows - 1; y >= 0; y--)
 {
     int maxHeightX = 0;
-    for (int y = grid.GetUpperBound(0); y >= 0; y--)
+    for (int x = columns - 1; x >= 0; x--)
     {
-        var tree = grid[x, y];
+        var tree = grid[y, x];
 
-        if (tree.Height > maxHeightX)
+        if (maxHeightY[x] < tree.Height)
         {
+            maxHeightY[x] = tree.Height;
             tree.IsVisible = true;
-            maxHeightX = tree.Height;
         }
 
-        if (tree.Height > maxHeightY)
+        if (maxHeightX < tree.Height)
         {
-            tree.IsVisible = true;
             maxHeightX = tree.Height;
+            tree.IsVisible = true;
         }
     }
 }
-
-for (int x = 0; x <= grid.GetUpperBound(1); x++)
-{
-    for (int y = 0; y <= grid.GetUpperBound(0); y++)
-    {
-        Console.Write($"{(grid[x,y].IsVisible ? 1 : 0)} ");
-    }
-    Console.Write(Environment.NewLine);
-}
-
-// for (int x = 0; x <= grid.GetUpperBound(1); x++)
-// {
-//     for (int y = 0; y <= grid.GetUpperBound(0); y++)
-//     {
-//         Console.Write($"{grid[x,y].Height} ");
-//     }
-//     Console.Write(Environment.NewLine);
-// }
 
 int totalVisible = 0;
-for (int x = 0; x <= grid.GetUpperBound(1); x++)
+for (int y = 0; y <= grid.GetUpperBound(1); y++)
 {
-    for (int y = 0; y <= grid.GetUpperBound(0); y++)
+    for (int x = 0; x <= grid.GetUpperBound(0); x++)
     {
-        totalVisible += grid[x, y].IsVisible ? 1 : 0;
+        totalVisible += grid[y, x].IsVisible ? 1 : 0;
     }
 }
-
 Console.WriteLine($"{totalVisible} trees are visible");
 
 record Tree(int Height, bool IsVisible = false)
