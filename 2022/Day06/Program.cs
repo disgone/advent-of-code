9304d149d756dd3107f6d67e4875c982a5193e35
Shaun Smith <disgone@gmail.com>
2022-12-08 00:38:41 -0600

    Add solution for day 6, part 1

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,30 @@
+﻿var dataStream = File.ReadAllText("input.txt");
+
+int GetMarkerPosition(ReadOnlySpan<char> text, int length)
+{
+    bool HasDuplicate(char needle, ReadOnlySpan<char> haystack)
+    {
+        foreach (var token in haystack)
+        {
+            if (needle == token) return true;
+        }
+
+        return haystack.Length > 1 && HasDuplicate(haystack[0], haystack.Slice(1));
+    }
+    
+    for (int i = 0; i < text.Length - length; i++)
+    {
+        var window = text.Slice(i, length);
+
+        if (!HasDuplicate(window[0], window.Slice(1)))
+        {
+            return i + length;
+        }
+    }
+
+    throw new InvalidOperationException("Could not find unique marker");
+}
+
+int markerPosition = GetMarkerPosition(dataStream.AsSpan(), 4);
+
+Console.WriteLine($"first marker after character {markerPosition}");
