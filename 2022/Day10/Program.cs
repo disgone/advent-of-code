64a556c53b77d6a8227c5cedea51132e665a955e
Shaun Smith <disgone@gmail.com>
2022-12-14 00:51:50 -0600

    Add solution for day 10, part 1

---
+++ 2022/Day10/Program.cs
@@ -0,0 +1,34 @@
+﻿Queue<string> instructions = new();
+foreach (var line in File.ReadAllLines("input.txt"))
+{
+    instructions.Enqueue(line);
+}
+
+long registerValue = 1;
+long cycle = 1;
+List<long> checkPoints = new();
+while (instructions.TryDequeue(out string current))
+{
+    int burn = 1;
+    int delta = 0;
+    if (current != "noop")
+    {
+        burn = 2;
+        
+        delta = int.Parse(current.Split(' ')[1]);
+    }
+
+    for (int i = 1; i <= burn; i++)
+    {
+        if (cycle == 20 || (cycle - 20) % 40 == 0)
+        {
+            checkPoints.Add(registerValue * cycle);
+        }
+        cycle++;
+    }
+
+    registerValue += delta;
+}
+
+Console.WriteLine(String.Join(',', checkPoints));
+Console.WriteLine(checkPoints.Sum());
